f479e42139e2de425c1dc799889c0f5d3b556ff5
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-07 19:08:16 -0300

    Instalados paquetes Swal2 para React, reformulado Layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,9 +1,29 @@
 import React from "react";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
 
 class Layout extends React.Component {
+    constructor() {
+        super();
+        
+        this.openDialogBox = this.openDialogBox.bind(this);
+    }
+
+    openDialogBox() {
+        MySwal.fire(
+            <p>Hola, mundo!</p>
+        );
+    }
+
     render() {
         return (
-            <p>Hola, mundo!</p>
+            <div>
+                <button className="btn btn-primary" onClick={this.openDialogBox}>
+                    <i className="fas fa-external-link-alt fa-fw"></i> Abrir cuadro de diálogo
+                </button>
+            </div>
         );
     }
 }
